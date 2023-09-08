76b176e4c7f01001eeaded231629ece007201a81
Chethanshetty4 <118925283+Chethanshetty4@users.noreply.github.com>
2023-09-08 09:35:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ button = getId('btn'),
 errMsg = getClasses('error'),
 sts = getId('status');
 
-form.style.backgroundColor = "#C2FABD";
+form.style.backgroundColor = "#E9FCE7";
 
 // console.log(errMsg);  //returns HTMLCollection(4) [div.error, div.error, div.error, div.error]
 
